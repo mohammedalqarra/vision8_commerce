c172dd998c76f5363fe433a4f187ef0b96917676
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-02-05 20:31:15 +0200

    init:commit laravel ui

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,7 @@ Route::prefix(LaravelLocalization::setLocale())->group(function(){
 });
 
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
