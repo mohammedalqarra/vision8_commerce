eda0e25cf86f6a36cac2b0f21eb567e0fbed7496
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-03-05 22:44:11 +0200

    init:commit edit store  APi

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Models\Image;
 use App\Models\Product;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -30,7 +32,7 @@ class ProductController extends Controller
                 'message' => 'No Data Found',
                 'status' => 'Success',
                 'data' => [],
-            ] , 200 );
+            ] , 404 );
         }
 
         return Product::all();
@@ -45,6 +47,71 @@ class ProductController extends Controller
     public function store(Request $request)
     {
         //
+        $request->validate([
+            'name_en' => 'required',
+            'name_ar' => 'required',
+            'image' => 'required',
+            'content_en' => 'required',
+            'content_ar' => 'required',
+            'price' => 'required',
+            'quantity' => 'required',
+            'category_id' => 'required',
+        ]);
+
+        //    $img = $request->file('image');
+        $img_name = rand() . $request->file('image')->getClientOriginalName();
+        $request->file('image')->move(public_path('uploads/products'), $img_name);
+
+
+        // convert name  and content to json
+        $name = json_encode([
+            'en' => $request->name_en,
+            'ar' => $request->name_ar,
+        ], JSON_UNESCAPED_UNICODE);
+
+
+        $content = json_encode([
+            'en' => $request->content_en,
+            'ar' => $request->content_ar,
+        ], JSON_UNESCAPED_UNICODE);
+
+        // Insert To DataBase
+
+        $slugCount = Product::where('slug', 'like', '%' . Str::slug($request->name_en) . '%')->count();
+
+        $slug = Str::Slug($request->name_en);
+
+        if ($slugCount) {
+            $slug = Str::Slug($request->name_en) . '_' . $slugCount;
+        }
+
+        $product = Product::create([
+            'name' => $name,
+            'slug' => $slug,
+            // 'slug' => Str::slug($request->name_en),
+            //  'slug' => Str::slug($request->name_en).rand(), // لما بدي اضيف أي منتج نفس الأسم بس ال id  تختلف
+            'image' => $img_name,
+            'content' => $content,
+            'price' => $request->price,
+            'sale_price' => $request->sale_price,
+            'quantity' => $request->quantity,
+            'category_id' => $request->category_id,
+        ]);
+
+
+        // uploads Album to images table if exists
+        if ($request->has('album')) {
+            foreach ($request->album as $item) {
+                $img_name = rand() . $item->getClientOriginalName();
+                $item->move(public_path('uploads/products'), $img_name);
+                Image::create([
+                    'path' => $img_name,
+                    'product_id' => $product->id,
+                ]);
+            }
+        }
+
+        return $product;
     }
 
     /**
@@ -56,6 +123,8 @@ class ProductController extends Controller
     public function show($id)
     {
         //
+
+        
     }
 
     /**
