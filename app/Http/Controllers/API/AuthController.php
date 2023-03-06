1d614727301c08902edfc849dcd3842f8a6b1819
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-03-06 15:05:14 +0200

    init:commit authlogin api

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -6,13 +6,46 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     //
     public function login(Request $request)
     {
-        
-    }
+        // $user = User::where('email' , $request->email)->first();
 
+        $user = User::where('email', $request->email)->first();
+        if($user) {
+
+            if(Hash::check($request->password, $user->password)) {
+
+                Auth::login($user);
+
+                $token = $user->createToken('login')->plainTextToken;
+
+                return response()->json([
+                    'message' => 'Login Successfully',
+                    'status' => 'Success',
+                    'data' => [
+                        'token' => $token
+                    ]
+                ], 200);
+
+            }else {
+                return response()->json([
+                    'message' => 'Password does Not Match',
+                    'status' => 'Success',
+                    'data' => []
+                ], 200);
+            }
+
+        }else {
+            return response()->json([
+                'message' => 'No User Found',
+                'status' => 'Success',
+                'data' => []
+            ], 404);
+        }
+    }
 }
