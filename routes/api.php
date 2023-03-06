ff24e7e7dd3bd8b4998dc7e436b705e2c3e35259
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-03-06 14:16:26 +0200

    init:commit apisanctum

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/login' , [AuthController::class , 'login']);
 Route::post('/register' , [AuthController::class , 'register']);
 //middleware('auth:sanctum')->
-Route::prefix('v1')->group(function (){ // كل اروابط تكون محمية من ال token
+Route::prefix('v1')->middleware('auth:sanctum')->group(function (){ // كل اروابط تكون محمية من ال token
     // Route::get('products' , function(){
     //     return 'ddd';
     // });
